8fd2aa9020fd90a780e40f511d797544a94c1864
Saima Rahman <rahman.saima92@gmail.com>
2019-10-30 18:32:09 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -11,24 +11,11 @@ end
 
 def print_first_directors_movie_titles
   array = directors_database
-  i = 0 
+  i = 0
   
   while i < array.length do
-    arr2 = array[i]
-    j = 0 
-    
-  while j < arr2.length do
-    arr3 = arr2[j]
-    k = 0 
-    
-  while k < arr3.length do
-   obj = arr3[k]
-   names = obj[:name]
-   puts names
-   k += 1
-  end
-    j += 1
-  end
-  i += 1
-  end
+    obj = array[i]
+    names = obj[:name]
+    puts names
+end
 end
